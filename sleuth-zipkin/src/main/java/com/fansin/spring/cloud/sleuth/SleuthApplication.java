5ec4f08985dd391b3adbdeaefd49840eddf4c34c
fansin <171388204@qq.com>
2018-01-10 10:54:43 +0800

    spring-cloud-sleuth-zipkin 全链路日志追踪,zipkin提供数据可视化

---
+++ sleuth-zipkin/src/main/java/com/fansin/spring/cloud/sleuth/SleuthApplication.java
@@ -0,0 +1,18 @@
+package com.fansin.spring.cloud.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@EnableZipkinStreamServer
+@EnableDiscoveryClient
+public class SleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthApplication.class, args);
+	}
+}
