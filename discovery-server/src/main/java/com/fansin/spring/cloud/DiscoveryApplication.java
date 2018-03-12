5202ef80503d06d24f5637a205c628d73c26a21c
FansinZhao <171388204@qq.com>
2018-03-12 17:00:10 +0800

    discovery-server 升级springboot2.0

--- discovery-server/src/main/java/com/fansin/spring/cloud/DiscoveryApplication.java
+++ discovery-server/src/main/java/com/fansin/spring/cloud/DiscoveryApplication.java
@@ -3,12 +3,19 @@ package com.fansin.spring.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 /**
+ * 基于AP(CAP理论)的peer-to-peer服务发现Eurake(有趣小知识:古希腊语:阿基米德发现浮力定律时,我发现了)
+ * 特点:
+ * 	AP: peer-to-peer,客户端缓存,自我保护
+ * 	restful
+ *
+ * vs consul vs zookeeper
+ *
+ * consul: raft,一种用于保证命令
+ *
+ *
+ *
+ *
  * @author fansin
  */
 @SpringBootApplication
@@ -16,7 +23,6 @@ import java.io.InputStream;
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
 }
